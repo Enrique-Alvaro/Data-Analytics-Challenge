fe28d035f3ca244f2fe18383a59398c966c768a8
Enrique-Alvaro <enriquealvaro97@gmail.com>
2022-03-31 09:03:54 -0300

    Agrego comentarios para Interpretabilidad

--- constanst.py
+++ constanst.py
@@ -1,14 +1,18 @@
 from pathlib import Path
 
+#Donde voy a poner mis archivos
 BASE_FILE_DIR = Path('/tmp')
 
+# Ubicacion de mi proyecto
 # Resolve me da la ubicacion actual
 # parent me da la carpeta madre de la carpeta actual
 ROOT_DIR = Path().resolve().parent
-#creo ruta para el SQL
+
+#creo ruta para la ubicacion de mis archivos SQL
 SQL_DIR = ROOT_DIR / 'challenge/sql'
 
 
+#Nombres de las tablas para Sql
 
 RAW_TABLE_NAME = 'raw'
 CINE_INSIGHTS_TABLE_NAME = 'cine_insights'
