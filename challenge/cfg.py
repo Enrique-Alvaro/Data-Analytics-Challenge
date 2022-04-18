b45099b1b5a386162d560d711d55e69de6b1a9f4
Enrique-Alvaro <enriquealvaro97@gmail.com>
2022-04-18 18:23:33 -0300

    limpieza de codigo

--- challenge/cfg.py
+++ challenge/cfg.py
@@ -1,35 +1,24 @@
 from decouple import AutoConfig
 from constanst import  ROOT_DIR
 
-
-# instancio config diciendole donde tiene que buscar los valores para las variables
-# levanto las variables
 config = AutoConfig(search_path = ROOT_DIR)
-
-# levanto las variables
 
 URL_DB = config('URL_DB' )
 MUSEO_URL = config('MUSEO_URL')
 CINES_URL = config('CINES_URL')
 ESPACIOS_URL = config('ESPACIOS_URL')
 
-
-#creo diccionario
-
 museo_ds = {
-    #nombre de mi data_source
     'name' : 'museo',
     'url': MUSEO_URL,
 }
 
 cines_ds = {
-    #nombre de mi data_source
     'name' : 'cines_',
     'url': CINES_URL,
 }
 
 espacios_ds = {
-    #nombre de mi data_source
     'name' : 'espacios',
     'url': ESPACIOS_URL,
 }
